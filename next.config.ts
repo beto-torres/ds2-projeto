e3c037e88a94039384eaf8172200893574b98f4b
beto <capb1981@gmail.com>
2026-03-25 07:09:42 -0300

    primeiro commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Permite qualquer domínio com HTTPS
+      },
+      {
+        protocol: 'http',
+        hostname: '**', // Permite qualquer domínio com HTTP
+      },
+    ],
+  },
 };
 
 export default nextConfig;
