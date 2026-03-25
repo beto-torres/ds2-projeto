ca3b45049ce604b9e179030c1dc2c284ea677da8
beto <capb1981@gmail.com>
2026-03-25 14:36:23 -0300

    aula 25/03/2026 ficou para descobrir como deixar a imagem como círculo

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    // teste
     <div className="flex gap-20 flex-wrap mx-auto w-5xl">
       <Image
         src={'https://images.unsplash.com/photo-1673300187070-4170c4c60aaa'}
